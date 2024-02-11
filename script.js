ba6cdb8b0e4c7938832a1721c337b85e7ff18ba3
chaudhary64 <chiragchoudhary64@gmail.com>
2024-02-11 18:36:02 +0530

    Animated the divs under Moving Divs comment in Html File

--- script.js
+++ script.js
@@ -9,3 +9,26 @@ a.forEach((e,index) => {
         b[index].style.top = '100%';
     });
 });
+
+let mDivOne = document.querySelector('.mDivOne');
+let mDivTwo = document.querySelector('.mDivTwo');
+
+gsap.to('.mDivOne p', {
+    x: 80, scrollTrigger: {
+        trigger: ".mDivOne",
+        scroll: "body",
+        scrub: 5,
+        start: "top 60%",
+        end: "-10% 14%",
+    }
+})
+
+gsap.to('.mDivTwo p', {
+    x: 80, scrollTrigger: {
+        trigger: ".mDivTwo",
+        scroll: "body",
+        scrub: 5,
+        start: "top 60%",
+        end: "-15% 14%",
+    }
+})
